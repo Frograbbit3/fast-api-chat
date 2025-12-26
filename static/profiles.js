a6b0d81e31e999d2665bc8d85d1a5eee3d9e6866
scatty <yourbl0ckedagain@gmail.com>
2025-12-26 13:46:12 -0800

    more setup ig

---
+++ static/profiles.js
@@ -0,0 +1,2 @@
+// not coding this yet, just need the confirmation that the backend is done =w=
+// note: if i end up coding this, just know the link will be like:   http://127.0.0.1:5000/profile?u=[USER]/
